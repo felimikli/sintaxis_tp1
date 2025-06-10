c1d5892358db43b5deee9e69235123c55b0b06e0
Felimikli <miklikowskifr@gmail.com>
2025-06-09 21:52:00 -0300

    agregar comentario con expersiones regulares

--- lexer.py
+++ lexer.py
@@ -6,6 +6,10 @@ LETRAS_MIN = set("abcdefghijklmnopqrstuvwxyz")
 LETRAS_MAY = set("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 NUMEROS = set("0123456789")
 
+# id regex (a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z|A|B|C|D|E|F|G|H|I|J|K|L|M|N|O|P|Q|R|S|T|U|V|W|X|Y|Z)(a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z|A|B|C|D|E|F|G|H|I|J|K|L|M|N|O|P|Q|R|S|T|U|V|W|X|Y|Z|0|1|2|3|4|5|6|7|8|9)*
+# num regex (-|ϵ)(0|1|2|3|4|5|6|7|8|9)*
+
+
 # Lista de tuples del nombre de token y su funcion asociada
 # PR -> palabra reservada
 # SP -> signo de puntuacion
@@ -66,7 +70,7 @@ def afd_palabra(lexema, palabra):
         funcion_estados = []
 
         for i, c  in enumerate(palabra):
-                funcion_estados.append({c: (i + 1)})
+                funcion_estados.append({c: (i + 1)}) # Llenar funcion de estados basado en palabra reducida. i = indice, c = caracter
 
         for char in lexema:
                 transicion = funcion_estados[estado]
